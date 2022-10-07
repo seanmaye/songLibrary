f79039974fb6c4a9f64bc9d79b0ebb0200b77906
SEAN MAYE <sam710@scarletmail.rutgers.edu>
2022-10-07 21:16:29 +0000

    implemented does aynthign else need to be done

--- DeleteScreenController.java
+++ DeleteScreenController.java
@@ -19,13 +19,26 @@ public class DeleteScreenController {
 	private Scene scene;
 	private Stage stage;
 	private Parent root;
+	private Song removeSong;
 
-	
-	//Switches back to main screen
+	public void initialize() {
+		removeSong = HomeScreenController.getPassSong();
+	}
+
+	// Switches back to main screen
 	public void confirm(ActionEvent e) throws IOException {
-		
+		HomeScreenController.removeElement(removeSong);
 		Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public void goBack(ActionEvent e) throws IOException {
+
+		Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
