be2e1936ff416d57a8905d939adfbfacca68f2f6
SEAN MAYE <sam710@scarletmail.rutgers.edu>
2022-10-07 01:11:14 +0000

    Controls addScreenfx.fxml it only goes back to the home screen right now bc im having trouble passing info through the scene

---
+++ AddScreenController.java
@@ -0,0 +1,33 @@
+package songlibrary;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class AddScreenController {
+
+	@FXML
+	private Label label;
+	private Scene scene;
+	private Stage stage;
+	private Parent root;
+
+	
+	//Switches back to main screen
+	public void confirm(ActionEvent e) throws IOException {
+		
+		Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
