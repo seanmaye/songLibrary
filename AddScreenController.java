600962b16c62a8cc1373ff1c4033266147f95ab2
SEAN MAYE <sam710@scarletmail.rutgers.edu>
2022-10-07 21:14:45 +0000

    implemented but DOESNT check for errors or removes trailing spaces

--- AddScreenController.java
+++ AddScreenController.java
@@ -10,22 +10,42 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AddScreenController {
 
+	@FXML
+	private TextField songField;
+	@FXML
+	private TextField albumField;
+	@FXML
+	private TextField yearField;
+	@FXML
+	private TextField artistField;
 	@FXML
 	private Label label;
 	private Scene scene;
 	private Stage stage;
 	private Parent root;
 
-	
-	//Switches back to main screen
+	// Switches back to main screen
 	public void confirm(ActionEvent e) throws IOException {
-		
+		String song = songField.getText();
+		String album = albumField.getText();
+		String artist = artistField.getText();
+		int year;
+		// try{
+		year = Integer.parseInt(yearField.getText());
+		// } catch(NumberFormatException ex){ // handle your exception
+
+		// }
+
+		Song test = new Song(song, artist, album, year);
+		HomeScreenController.addElement(test);
+
 		Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
-		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
