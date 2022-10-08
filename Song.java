95c0876ba0af65b7fe253f124da3964208603f44
SEAN MAYE <sam710@scarletmail.rutgers.edu>
2022-10-08 20:49:45 +0000

    added other constructors

--- Song.java
+++ Song.java
@@ -6,7 +6,21 @@ public class Song {
 	private String artist;
 	private String album;
 	private int year;
-
+	
+	public Song(String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+	}
+	public Song(String name, String artist, String album) {
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+	}
+	public Song(String name, String artist, int year) {
+		this.name = name;
+		this.artist = artist;
+		this.year = year;
+	}
 	public Song(String name, String artist, String album, int year) {
 		this.name = name;
 		this.artist = artist;
