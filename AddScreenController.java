63c7cfff0371835447aeb64ab187b4a8cc3a084a
SEAN MAYE <sam710@scarletmail.rutgers.edu>
2022-10-08 20:47:58 +0000

    added stuff to catch errors (i still need to make pop ups tho)

--- AddScreenController.java
+++ AddScreenController.java
@@ -29,25 +29,70 @@ public class AddScreenController {
 	private Stage stage;
 	private Parent root;
 
-	// Switches back to main screen
+	// Adds item to list if the song meets the correct parameters
 	public void confirm(ActionEvent e) throws IOException {
-		String song = songField.getText();
-		String album = albumField.getText();
-		String artist = artistField.getText();
-		int year;
-		// try{
-		year = Integer.parseInt(yearField.getText());
-		// } catch(NumberFormatException ex){ // handle your exception
 
-		// }
+		int intTest;
+		// tests if Input to year is an integer
+		if (yearField.getText().trim().isEmpty() == false) {
+			try {
+				intTest = Integer.parseInt(yearField.getText());
+				// Checks if Song or album field are empty
+			} catch (NumberFormatException ex) {
+				System.out.println("POP UP0");
+			}
+		}
 
-		Song test = new Song(song, artist, album, year);
-		HomeScreenController.addElement(test);
+		if ((songField.getText() == null || songField.getText().trim().isEmpty())
+				|| (artistField.getText() == null || artistField.getText().trim().isEmpty())) {
+			System.out.println("POP UP1");
+			// checks if year is less than 0
+		} else if (yearField.getText().trim().isEmpty() == false && Integer.parseInt(yearField.getText().trim()) <= 0) {
+			System.out.println("POP UP2");
+			// checks if | character is in album/song/artist name
+		} else if (artistField.getText().indexOf('|') != -1
+				|| ((albumField.getText() == null || albumField.getText().trim().isEmpty()) == false
+				&& albumField.getText().indexOf('|') != -1)
+		|| songField.getText().indexOf('|') != -1) {
+			System.out.println("POP UP3");
+			// adds song to list
+		} else {
+			Song test;
+			if ((albumField.getText() == null || albumField.getText().trim().isEmpty())
+					&& (yearField.getText() == null || yearField.getText().trim().isEmpty())) {
+				String song = songField.getText().trim();
+				String artist = artistField.getText().trim();
+				test = new Song(song, artist);
+			} else if (albumField.getText() == null || albumField.getText().trim().isEmpty()) {
+				String song = songField.getText().trim();
+				String artist = artistField.getText().trim();
+				int year = Integer.parseInt(yearField.getText().trim());
+				test = new Song(song, artist, year);
+			} else if (yearField.getText() == null || yearField.getText().trim().isEmpty()) {
+				String song = songField.getText().trim();
+				String artist = artistField.getText().trim();
+				String album = albumField.getText().trim();
+				test = new Song(song, artist, album);
+			} else {
+				String song = songField.getText().trim();
+				String artist = artistField.getText().trim();
+				String album = albumField.getText().trim();
+				int year = Integer.parseInt(yearField.getText().trim());
+				test = new Song(song, artist, album, year);
+			}
 
-		Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
-		stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
+			if (HomeScreenController.checkElement(test) == true) {
+				System.out.println("Already in list (popup)");
+			} else {
+				HomeScreenController.addElement(test);
+				Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
+				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+				scene = new Scene(root);
+				stage.setScene(scene);
+				stage.show();
+			}
+
+		}
+
 	}
 }
