ee62bbcadca48c7429f6770046c2d0ee315fd54d
SEAN MAYE <sam710@scarletmail.rutgers.edu>
2022-10-09 21:45:38 +0000

    added popups

--- AddScreenController.java
+++ AddScreenController.java
@@ -4,11 +4,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -34,27 +38,37 @@ public class AddScreenController {
 
 		int intTest;
 		// tests if Input to year is an integer
-		if (yearField.getText().trim().isEmpty() == false) {
+		if ((songField.getText() == null || songField.getText().trim().isEmpty())
+				|| (artistField.getText() == null || artistField.getText().trim().isEmpty())) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("ERROR");
+			alert.setContentText("Song needs at least a NAME and a ARTIST");
+			alert.showAndWait();
+			// checks if year is less than 0
+		} else if (yearField.getText().trim().isEmpty() == false) {
 			try {
 				intTest = Integer.parseInt(yearField.getText());
 				// Checks if Song or album field are empty
 			} catch (NumberFormatException ex) {
-				System.out.println("POP UP0");
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("ERROR");
+				alert.setContentText("Year must be a POSITIVE integer");
+				alert.showAndWait();
 			}
-		}
-
-		if ((songField.getText() == null || songField.getText().trim().isEmpty())
-				|| (artistField.getText() == null || artistField.getText().trim().isEmpty())) {
-			System.out.println("POP UP1");
-			// checks if year is less than 0
 		} else if (yearField.getText().trim().isEmpty() == false && Integer.parseInt(yearField.getText().trim()) <= 0) {
-			System.out.println("POP UP2");
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("ERROR");
+			alert.setContentText("Year must be a POSITIVE integer");
+			alert.showAndWait();
 			// checks if | character is in album/song/artist name
 		} else if (artistField.getText().indexOf('|') != -1
 				|| ((albumField.getText() == null || albumField.getText().trim().isEmpty()) == false
-				&& albumField.getText().indexOf('|') != -1)
-		|| songField.getText().indexOf('|') != -1) {
-			System.out.println("POP UP3");
+						&& albumField.getText().indexOf('|') != -1)
+				|| songField.getText().indexOf('|') != -1) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("ERROR");
+			alert.setContentText("Invalid character: | ");
+			alert.showAndWait();
 			// adds song to list
 		} else {
 			Song test;
@@ -82,14 +96,28 @@ public class AddScreenController {
 			}
 
 			if (HomeScreenController.checkElementAdd(test) == true) {
-				System.out.println("Already in list (popup)");
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("ERROR");
+				alert.setContentText("Song is already in list!");
+				alert.showAndWait();
 			} else {
-				HomeScreenController.addElement(test);
-				Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
-				stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-				scene = new Scene(root);
-				stage.setScene(scene);
-				stage.show();
+				Alert alert = new Alert(AlertType.CONFIRMATION);
+				alert.setTitle("Confirm Action");
+				String s = "Press OK to add the song to the list";
+				alert.setContentText(s);
+
+				Optional<ButtonType> result = alert.showAndWait();
+
+				if ((result.isPresent()) && (result.get() == ButtonType.OK)) {
+					HomeScreenController.addElement(test);
+					Parent root = FXMLLoader.load(getClass().getResource("homeScreenfx.fxml"));
+					stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+					scene = new Scene(root);
+					stage.setScene(scene);
+					stage.show();
+
+				}
+
 			}
 
 		}
